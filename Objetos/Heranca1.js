3958aba5ed7e5107af67b35cf419778591394922
bragaus <mannoplay@gmail.com>
2020-09-27 13:39:45 -0300

    Herança #01

---
+++ Objetos/Heranca1.js
@@ -0,0 +1,9 @@
+var ferrari = {
+    nome: 'ferrar',
+    price: 100000
+}
+
+console.log(ferrari.__proto__);
+console.log(ferrari.__proto__ === Object.prototype);
+console.log(Object.prototype);
+console.log(Object.prototype.__proto__);
