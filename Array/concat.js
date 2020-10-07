635b34e171e6e050a9ccebd7c2f87dba2f5ba340
bragaus <mannoplay@gmail.com>
2020-10-06 21:21:29 -0300

    .\concat.js

---
+++ Array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeska', 'Cibalena']
+const filhos = ['Uoxiton', 'Uescley']
+const todos = filhas.concat(filhos)
+console.log(todos)
+
+console.log(['a', 'b'].concat([1,2],[3,4],[5,6],[[7,8]]))
