4958c0db3ab6195a22773a578c8c5e6693938b25
bragaus <mannoplay@gmail.com>
2020-10-09 22:00:39 -0300

    Projeto: API com express #03

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,15 +2,40 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'SimsTim', preco: 2222.45 }) // Converter para JSON
-    next()
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON
 })
 
-app.get('/produtos', (req, res, next) => {
-    //res.send({ nome: 'Notebook', preco: 123.45 }) // Converter para JSON
-    console.log({ nome: 'Notebook', preco: 123.45 })
+app.get('/produtos/:id', (req, res) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProdutos({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProdutos({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
